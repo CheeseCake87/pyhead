d956d8313d546d21a67ce6b5bb9f444324ae1b18
David <carmichaelits@gmail.com>
2025-07-23 10:12:51 +0100

    remove unused function

--- src/pyhead/_helpers.py
+++ src/pyhead/_helpers.py
@@ -1,22 +1,6 @@
-from string import punctuation, whitespace
-
-
 def has_key(element):
     if hasattr(element, "key"):
         if element.key:
             return element.key
 
     return None
-
-
-def replace_special_chars(value: str):
-    """
-    Replace special characters in a string.
-
-    Returns:
-        str: String with special characters replaced.
-    """
-    special_chars = [punctuation, whitespace]
-    for special_char in special_chars:
-        value = value.replace(special_char, "-")
-    return value
