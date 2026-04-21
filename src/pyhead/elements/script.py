5c5d40b3bfc7f3ae1f07ac0fb803bc3489d5c61c
David <carmichaelits@gmail.com>
2026-04-21 20:59:00 +0100

    escape attribute values in Script tag to ensure safe HTML rendering

--- src/pyhead/elements/script.py
+++ src/pyhead/elements/script.py
@@ -1,23 +1,23 @@
 from typing import Optional, Union
 
-from markupsafe import Markup
+from markupsafe import Markup, escape
 
 from ..protocols import CompileDelayed
 
 
 class Script:
     unique: bool = False
-    key: str = None
+    key: Optional[str] = None
 
-    _src: str
-    _type: str
+    _src: Union[str, CompileDelayed]
+    _type: Optional[str]
     _async: bool
     _defer: bool
-    _crossorigin: str
-    _integrity: str
+    _crossorigin: Optional[str]
+    _integrity: Optional[str]
     _nomodule: bool
-    _referrerpolicy: str
-    _id: str
+    _referrerpolicy: Optional[str]
+    _id: Optional[str]
 
     def __init__(
         self,
@@ -57,22 +57,27 @@ class Script:
         __items = []
 
         if self._src:
-            __items.append(f'src="{self._src}"')
+            src = (
+                self._src.compile()
+                if isinstance(self._src, CompileDelayed)
+                else self._src
+            )
+            __items.append(f'src="{escape(src)}"')
         if self._type:
-            __items.append(f'type="{self._type}"')
+            __items.append(f'type="{escape(self._type)}"')
         if self._async:
             __items.append(f'async="{str(self._async).lower()}"')
         if self._defer:
             __items.append(f'defer="{str(self._defer).lower()}"')
         if self._crossorigin:
-            __items.append(f'crossorigin="{self._crossorigin}"')
+            __items.append(f'crossorigin="{escape(self._crossorigin)}"')
         if self._integrity:
-            __items.append(f'integrity="{self._integrity}"')
+            __items.append(f'integrity="{escape(self._integrity)}"')
         if self._nomodule:
             __items.append(f'nomodule="{str(self._nomodule).lower()}"')
         if self._referrerpolicy:
-            __items.append(f'referrerpolicy="{self._referrerpolicy}"')
+            __items.append(f'referrerpolicy="{escape(self._referrerpolicy)}"')
         if self._id:
-            __items.append(f'id="{self._id}"')
+            __items.append(f'id="{escape(self._id)}"')
 
         return f"<script {' '.join(__items)}></script>"
