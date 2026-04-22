03c2591d9452dd0df9df7a8a69a9a32b37de1520
David <carmichaelits@gmail.com>
2026-04-22 09:26:46 +0100

    refactor `Title` class to inherit from `BaseElement` and simplify methods

--- src/pyhead/elements/title.py
+++ src/pyhead/elements/title.py
@@ -1,25 +1,18 @@
-from markupsafe import Markup, escape
+from markupsafe import escape
+
+from .._base import BaseElement
 
 
-class Title:
+class Title(BaseElement):
     key: str = "title"
 
     _title: str
 
-    def __init__(
-        self,
-        title: str,
-    ) -> None:
+    def __init__(self, title: str) -> None:
         self._title = title
 
     def __repr__(self) -> str:
-        return f'<Title title="{self._title}">'
-
-    def __str__(self) -> Markup:
-        return Markup(f"{self.compile()}")
-
-    def __call__(self) -> Markup:
-        return Markup(f"{self.compile()}")
+        return f"Title(title={self._title!r})"
 
     def compile(self) -> str:
         return f"<title>{escape(self._title)}</title>"
